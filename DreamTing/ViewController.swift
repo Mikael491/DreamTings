61f4935e6c1c40ee8d7f37f11f642355dc6d7fda
Mikael <miketekle14@gmail.com>
2016-09-16 11:26:58 -0700

    Initial Commit

---
+++ DreamTing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DreamTing
+//
+//  Created by Mikael Teklehaimanot on 9/16/16.
+//  Copyright © 2016 Mikael Teklehaimanot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
