79617e6857ceab4c6daae7cdc9a7a762bb571cba
Mikael <miketekle14@gmail.com>
2016-09-17 17:27:47 -0700

    added shortcut for accessing app delegate and core data properties via app delegate

--- DreamTing/AppDelegate.swift
+++ DreamTing/AppDelegate.swift
@@ -92,3 +92,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+@available(iOS 10.0, *)
+let ad = UIApplication.shared.delegate as! AppDelegate
+
+@available(iOS 9.3, *)
+let context = ad.persistentContainer.viewContext
