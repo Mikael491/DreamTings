90be2fde2d418041eb0e11e3be0c3ff557b323d6
Mikael <miketekle14@gmail.com>
2016-09-18 15:06:04 -0700

    added image view and textflieds for entry input with custom textfield class for designing the view

--- DreamTing/MaterialAddorEdit.swift
+++ DreamTing/MaterialAddorEdit.swift
@@ -9,11 +9,20 @@
 import UIKit
 
 class MaterialAddorEdit: UIViewController {
+    
+    @IBOutlet weak var titleTxtFld: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        //print("This is the height: \(titleTxtFld.frame.size.height)")
     }
     
 }
