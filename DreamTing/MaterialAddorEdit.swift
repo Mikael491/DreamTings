bbffae96889084adc546a3d114c0431fd8ef0802
Mikael <miketekle14@gmail.com>
2016-09-18 14:29:47 -0700

    added new view controller object and subclass, connected them

---
+++ DreamTing/MaterialAddorEdit.swift
@@ -0,0 +1,19 @@
+//
+//  MaterialAddorEdit.swift
+//  DreamTing
+//
+//  Created by Mikael Teklehaimanot on 9/18/16.
+//  Copyright © 2016 Mikael Teklehaimanot. All rights reserved.
+//
+
+import UIKit
+
+class MaterialAddorEdit: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
